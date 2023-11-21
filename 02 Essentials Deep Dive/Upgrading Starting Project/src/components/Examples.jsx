2e4ebd2a652d542693d0a7b1fdc1281b4ccdaef7
Neal Matta <nealmatta@Neals-Mac-mini.local>
2023-11-20 23:32:18 -0600

    Finished section 65. Im not sure how often id use this but its a cool concept to know

--- 02 Essentials Deep Dive/Upgrading Starting Project/src/components/Examples.jsx
+++ 02 Essentials Deep Dive/Upgrading Starting Project/src/components/Examples.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import TabButton from './TabButton';
+import Section from './Section';
 import { EXAMPLES } from '../data';
+import Tabs from './Tabs';
 
 export default function Examples() {
 	// Since State is in it's own components, only this function is executed again when needed
@@ -27,35 +29,35 @@ export default function Examples() {
 	}
 
 	return (
-		<section id="examples">
+		<Section id="examples">
 			<h2>Examples</h2>
 			<menu>
 				<TabButton
 					isSelected={selectedTopic === 'components'}
-					onSelect={() => handleSelect('components')}
+					onClick={() => handleSelect('components')}
 				>
 					Components
 				</TabButton>
 				<TabButton
 					isSelected={selectedTopic === 'jsx'}
-					onSelect={() => handleSelect('jsx')}
+					onClick={() => handleSelect('jsx')}
 				>
 					JSX
 				</TabButton>
 				<TabButton
 					isSelected={selectedTopic === 'props'}
-					onSelect={() => handleSelect('props')}
+					onClick={() => handleSelect('props')}
 				>
 					Props
 				</TabButton>
 				<TabButton
 					isSelected={selectedTopic === 'state'}
-					onSelect={() => handleSelect('state')}
+					onClick={() => handleSelect('state')}
 				>
 					State
 				</TabButton>
 			</menu>
 			{tabContent}
-		</section>
+		</Section>
 	);
 }
