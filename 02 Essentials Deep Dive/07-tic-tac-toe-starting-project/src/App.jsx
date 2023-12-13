c1764fcca832ccf0456282c10efeb535c14074e2
Neal Matta <nlsima97@gmail.com>
2023-12-12 23:39:16 -0600

    87 - honestly a ton of changes. I kept all my comments in. Biggest thing I learned about was copying objects and arrays before modifying them. I added a link in the code that is a good resource

--- 02 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ 02 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,16 +1,66 @@
+import { useState } from 'react';
+
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
+import Log from './components/Log';
+
+function helperDeriveActivePlayer(gameTurns) {
+	let currentPlayer = 'X';
+
+	if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+		currentPlayer = 'O';
+	}
+
+	return currentPlayer;
+}
 
 function App() {
+	const [gameTurns, setGameTurns] = useState([]);
+
+	const activePlayer = helperDeriveActivePlayer(gameTurns);
+
+	function handleSelectSquare(rowIndex, colIndex) {
+		setGameTurns((prevTurns) => {
+			/* 83 - This approach for current player helps us ensure that we're not combining state values
+			let currentPlayer = 'X';
+
+			if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+				currentPlayer = 'O';
+			}*/
+
+			const currentPlayer = helperDeriveActivePlayer(prevTurns);
+			const updatedTurns = [
+				{
+					square: { row: rowIndex, col: colIndex },
+					player: currentPlayer,
+				},
+				...prevTurns,
+			];
+
+			return updatedTurns;
+		});
+	}
 	return (
 		<main>
 			<div id="game-container">
-				<ol id="players">
-					<Player initialName="Player 1" symbol="X" />
-					<Player initialName="Player 2" symbol="O" />
+				<ol id="players" className="highlight-player">
+					<Player
+						initialName="Player 1"
+						symbol="X"
+						isActive={activePlayer === 'X'}
+					/>
+					<Player
+						initialName="Player 2"
+						symbol="O"
+						isActive={activePlayer === 'O'}
+					/>
 				</ol>
-				GAME BOARD
+				<GameBoard
+					onSelectSquare={handleSelectSquare}
+					turns={gameTurns}
+				/>
 			</div>
-			LOG
+			<Log turns={gameTurns} />
 		</main>
 	);
 }
---
+++ 02 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,58 @@
+const initialGameBoard = [
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
+];
+
+export default function GameBoard({ onSelectSquare, turns }) {
+	let gameBoard = initialGameBoard;
+
+	// 84 - You should manage as little state as possible but rather derive values from a state
+	for (const turn of turns) {
+		const { square, player } = turn;
+		gameBoard[square.row][square.col] = player;
+	}
+
+	/* 82 - While this works, the data captured here needs to be referenced 
+    in multiple components. So rather than have two separate states, I can life 
+    this state to the parent component and then pass it to this component */
+
+	// const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+	// function handleSelectSquare(rowIndex, colIndex) {
+	// 	// 79 - If the state is an object or an array, you want to update it in an immutable way
+	// 	// https://academind.com/tutorials/reference-vs-primitive-values
+	// 	setGameBoard((prevGameBoard) => {
+	// 		const updatedBoard = [
+	// 			...prevGameBoard.map((innerArray) => [...innerArray]),
+	// 		];
+	// 		updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+	// 		return updatedBoard;
+	// 	});
+
+	// 	onSelectSquare();
+	// }
+
+	return (
+		<ol id="game-board">
+			{gameBoard.map((row, rowIndex) => (
+				<li key={rowIndex}>
+					<ol>
+						{row.map((playerSymbol, colIndex) => (
+							<li key={colIndex}>
+								<button
+									onClick={() =>
+										onSelectSquare(rowIndex, colIndex)
+									}
+									disabled={playerSymbol !== null}
+								>
+									{playerSymbol}
+								</button>
+							</li>
+						))}
+					</ol>
+				</li>
+			))}
+		</ol>
+	);
+}
