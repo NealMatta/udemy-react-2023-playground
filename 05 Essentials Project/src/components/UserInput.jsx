874566be9c2a7763b81b5360a8bc7d8ad07e4851
Neal Matta <nlsima97@gmail.com>
2023-12-20 20:24:15 -0600

    Better user input. Need to start the react process

--- 05 Essentials Project/src/components/UserInput.jsx
+++ 05 Essentials Project/src/components/UserInput.jsx
@@ -1,20 +1,20 @@
 export default function UserInput() {
 	return (
 		<div id="user-input">
-			<div className="input-group">
-				<label htmlFor="initialInvestment">Initial Investment</label>
+			<div>
+				<label>Initial Investment</label>
 				<input type="number" id="initialInvestment" />
 			</div>
-			<div className="input-group">
-				<label htmlFor="annualInvestment">Annual Investment</label>
+			<div>
+				<label>Annual Investment</label>
 				<input type="number" id="annualInvestment" />
 			</div>
-			<div className="input-group">
-				<label htmlFor="expectedReturn">Expected Return</label>
+			<div>
+				<label>Expected Return</label>
 				<input type="number" id="expectedReturn" />
 			</div>
-			<div className="input-group">
-				<label htmlFor="duration">Duration</label>
+			<div>
+				<label>Duration</label>
 				<input type="number" id="duration" />
 			</div>
 		</div>
