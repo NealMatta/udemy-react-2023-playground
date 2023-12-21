1a3fdf6a22cce9f438b3e554c074f8ddd020219d
Neal Matta <nlsima97@gmail.com>
2023-12-20 20:19:13 -0600

    Table in results via HTML is set up

--- 05 Essentials Project/src/components/Results.jsx
+++ 05 Essentials Project/src/components/Results.jsx
@@ -1,3 +1,24 @@
 export default function Results() {
-	return <div>Results</div>;
+	return (
+		<table id="result">
+			<thead>
+				<tr>
+					<th>Year</th>
+					<th>Investment Value</th>
+					<th>Interest (Year)</th>
+					<th>Total Interest</th>
+					<th>Invested Capital</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Test</td>
+					<td>Test</td>
+					<td>Test</td>
+					<td>Test</td>
+					<td>Test</td>
+				</tr>
+			</tbody>
+		</table>
+	);
 }
