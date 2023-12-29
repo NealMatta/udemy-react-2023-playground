d53818de0f4aba32eec0ba73993671c854e6333b
Neal Matta <nlsima97@gmail.com>
2023-12-28 21:25:12 -0600

    141 - Created an input approach using only state. Next section is about using ref

--- 08 Refs Portal/src/components/Player.jsx
+++ 08 Refs Portal/src/components/Player.jsx
@@ -1,11 +1,31 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	// const inputPlayerName = useRef();
+
+	const [playerName, setPlayerName] = useState('');
+	const [submitted, setSubmitted] = useState(false);
+
+	function handleChange(e) {
+		setPlayerName(e.target.value);
+	}
+
+	function handleClick() {
+		setSubmitted(true);
+	}
+
+	return (
+		<section id="player">
+			<h2>Welcome {submitted ? playerName : 'unknown entity'}</h2>
+			<p>
+				<input
+					ref={playerName}
+					type="text"
+					onChange={handleChange}
+					value={playerName}
+				/>
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
 }
