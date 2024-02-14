09315b6caf6b16faa2f94d40c27e53c301a7cd11
Neal Matta <nlsima97@gmail.com>
2024-02-13 23:08:30 -0600

    Finished section 196

--- 13 Demo Project - React Quiz/src/components/Quiz.jsx
+++ 13 Demo Project - React Quiz/src/components/Quiz.jsx
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import QUESTIONS from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from './QuestionTimer';
 
 export default function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([]);
 
 	const activeQuestionIndex = userAnswers.length;
+	// Checking if QUiz is Complete
+	const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-	function handleSelectAnswer(selectedAnswer) {
+	const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
 		setUserAnswers((prevUserAnswers) => {
 			return [...prevUserAnswers, selectedAnswer];
 		});
+	}, []);
+
+	const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), handleSelectAnswer);
+
+	if (quizIsComplete) {
+		return (
+			<div id="summary">
+				<img src={quizCompleteImg} alt="Trophy Icon" />
+				<h2>Quiz Compelted</h2>
+			</div>
+		);
 	}
+
+	// Randomizing the answers
+	const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+	shuffledAnswers.sort(() => Math.random() - 0.5);
 
 	return (
 		<div id="quiz">
 			<div id="question">
+				<QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
 				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 				<ul id="answers">
-					{QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+					{shuffledAnswers.map((answer) => {
 						return (
 							<li key={answer} className="answer">
 								<button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
