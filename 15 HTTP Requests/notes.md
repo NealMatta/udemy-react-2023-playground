aceba0fb810686024ee90b0e643dd53795b973f6
Neal Matta <nlsima97@gmail.com>
2024-04-09 22:55:18 -0500

    Finished up to section 235

--- 15 HTTP Requests/notes.md
+++ 15 HTTP Requests/notes.md
@@ -4,3 +4,18 @@
     -   The visitor will be able to access all the credentials to databases
     -   Optimization purposes too
 -   Rather, you want to communicate with a backend server
+
+# 232. Using async / await
+
+-   async/await can be used within useEffect but it can't be used **on** used effect
+
+# 233. Handling Loading States
+
+-   Loading is done via states. Kinda an annoying solution as I always want to be checking if data is loading or not
+-   There should be a package or something that exists
+
+# 234. Handling HTTP Errors
+
+-   Success responses are 200 or 300 while error response are 400 or 500
+-   Wrap code that could throw an error with try/catch
+-   **Fetching, Loading, and Error states are common states created in each component**
--- 15 HTTP Requests/src/components/AvailablePlaces.jsx
+++ 15 HTTP Requests/src/components/AvailablePlaces.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+	const [isFetching, setIsFetching] = useState(false);
 	const [availablePlaces, setAvailablePlaces] = useState([]);
+	const [error, setError] = useState();
 
+	/* Using .then 
 	useEffect(() => {
 		// Calling this in the function sets off an infinite loop. That's why you need to execute in a useEffect hook
 		fetch('http://localhost:3000/places')
@@ -14,11 +19,51 @@ export default function AvailablePlaces({ onSelectPlace }) {
 				setAvailablePlaces(resData.places);
 			});
 	}, []);
+	*/
+
+	// Using Asnyc/Await
+	// Standard way to grab data while also using errors and loading
+	useEffect(() => {
+		async function fetchPlaces() {
+			setIsFetching(true);
+
+			try {
+				const response = await fetch('http://localhost:3000/places');
+				const resData = await response.json();
+
+				if (!response.ok) {
+					throw new Error('Failed to fetch places');
+				}
+
+				// Can't use asnyc await because it doesn't yield a promise
+				navigator.geolocation.getCurrentPosition((position) => {
+					const sortedPlaces = sortPlacesByDistance(
+						resData.places,
+						position.coords.latitude,
+						position.coords.longitude
+					);
+					setIsFetching(false);
+					setAvailablePlaces(sortedPlaces);
+				});
+			} catch (error) {
+				setError({ message: error.message || 'Shit went wrong' });
+				setIsFetching(false);
+			}
+		}
+
+		fetchPlaces();
+	}, []);
+
+	if (error) {
+		return <Error title={'An Error occurred'} message={error.message} />;
+	}
 
 	return (
 		<Places
 			title="Available Places"
 			places={availablePlaces}
+			isLoading={isFetching}
+			loadingText="Loading... "
 			fallbackText="No places available."
 			onSelectPlace={onSelectPlace}
 		/>
