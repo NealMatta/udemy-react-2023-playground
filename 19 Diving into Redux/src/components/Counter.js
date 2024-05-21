ec17745ae744fba510ff1cd3cc2b3b05640c8ed2
Neal Matta <nlsima97@gmail.com>
2024-05-21 18:20:56 -0500

    Finished section 291

--- 19 Diving into Redux/src/components/Counter.js
+++ 19 Diving into Redux/src/components/Counter.js
@@ -1,15 +1,19 @@
+// useSelector helps us grab a part of the store
+import { useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+	// React Redux automatically sets up a subscription to the Redux Store so you'll always have the latest state value
+	const counter = useSelector((state) => state.counter);
+	const toggleCounterHandler = () => {};
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			<div className={classes.value}>{counter}</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;
