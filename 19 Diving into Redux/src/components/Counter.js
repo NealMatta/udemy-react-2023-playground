15fa02f642dac7e511fb70e0fe2ae431bd041df2
Neal Matta <nlsima97@gmail.com>
2024-05-21 22:10:01 -0500

    Completed section 295

--- 19 Diving into Redux/src/components/Counter.js
+++ 19 Diving into Redux/src/components/Counter.js
@@ -5,21 +5,22 @@ import classes from './Counter.module.css';
 const Counter = () => {
 	// React Redux automatically sets up a subscription to the Redux Store so you'll always have the latest state value
 	const counter = useSelector((state) => state.counter);
-	const dispatch = useDispatch();
-	const toggleCounterHandler = () => {};
+	const showCounter = useSelector((state) => state.showCounter);
 
-	// function incrementHandler() {
-	// 	dispatch({ type: 'increment', amount: 5 });
-	// }
+	const dispatch = useDispatch();
 
 	function decrementHandler() {
 		dispatch({ type: 'decrement' });
 	}
 
+	const toggleCounterHandler = () => {
+		dispatch({ type: 'toggle' });
+	};
+
 	return (
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
-			<div className={classes.value}>{counter}</div>
+			{showCounter && <div className={classes.value}>{counter}</div>}
 			<div>
 				<button onClick={() => dispatch({ type: 'increment', amount: 1 })}>Increment</button>
 				<button onClick={() => dispatch({ type: 'increment', amount: 5 })}>Increment by 5</button>
--- 19 Diving into Redux/src/store/index.js
+++ 19 Diving into Redux/src/store/index.js
@@ -1,15 +1,25 @@
 import { createStore } from 'redux';
+const initialState = { counter: 0, showCounter: true };
 
-function counterReducer(state = { counter: 0 }, action) {
+function counterReducer(state = initialState, action) {
 	if (action.type === 'increment') {
 		return {
 			counter: state.counter + action.amount,
+			showCounter: state.showCounter,
 		};
 	}
 
 	if (action.type === 'decrement') {
 		return {
 			counter: state.counter - 1,
+			showCounter: state.showCounter,
+		};
+	}
+
+	if (action.type === 'toggle') {
+		return {
+			counter: state.counter,
+			showCounter: !state.showCounter,
 		};
 	}
 
