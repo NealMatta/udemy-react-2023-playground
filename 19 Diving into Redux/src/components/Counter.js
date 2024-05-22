dc502199dd77a3372c3c25e53ddd560cdffac1b8
Neal Matta <nlsima97@gmail.com>
2024-05-21 22:01:09 -0500

    Action payload section

--- 19 Diving into Redux/src/components/Counter.js
+++ 19 Diving into Redux/src/components/Counter.js
@@ -8,6 +8,10 @@ const Counter = () => {
 	const dispatch = useDispatch();
 	const toggleCounterHandler = () => {};
 
+	// function incrementHandler() {
+	// 	dispatch({ type: 'increment', amount: 5 });
+	// }
+
 	function decrementHandler() {
 		dispatch({ type: 'decrement' });
 	}
@@ -17,7 +21,8 @@ const Counter = () => {
 			<h1>Redux Counter</h1>
 			<div className={classes.value}>{counter}</div>
 			<div>
-				<button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+				<button onClick={() => dispatch({ type: 'increment', amount: 1 })}>Increment</button>
+				<button onClick={() => dispatch({ type: 'increment', amount: 5 })}>Increment by 5</button>
 				<button onClick={decrementHandler}>Decrement</button>
 			</div>
 			<button onClick={toggleCounterHandler}>Toggle Counter</button>
