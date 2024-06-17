9a2b83b68a39872a6b01154dfcf10e33385c2e9e
Neal Matta <nlsima97@gmail.com>
2024-06-16 22:24:24 -0500

    learned the new ways to use react router dom and the proper wayto use LInk

---
+++ 21 Routing/Notes.md
@@ -0,0 +1,5 @@
+# 328. Navigating between pages with links
+
+-   A big purpose of react router is so that you don't have to request the javascript code again and again
+    -   Impact: Bad site performance
+    -   Using hrefs makes it so that you would
--- 21 Routing/src/pages/Home.js
+++ 21 Routing/src/pages/Home.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-	return <h1>My Home</h1>;
+	return (
+		<>
+			<h1>My Home</h1>
+			<p>
+				Go to the <Link to="/products"> list of products</Link>
+			</p>
+		</>
+	);
 }
