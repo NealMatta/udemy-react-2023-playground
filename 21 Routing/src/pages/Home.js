5783b65d5e39be0124ded68fb9fd0f622a23d481
Neal Matta <nlsima97@gmail.com>
2024-06-18 17:31:11 -0500

    Programatic navigation

--- 21 Routing/src/pages/Home.js
+++ 21 Routing/src/pages/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Home() {
+	const navigate = useNavigate();
+
+	// Example of programatic navigation
+	function navigateHandler() {
+		navigate('/products');
+	}
 	return (
 		<>
 			<h1>My Home</h1>
 			<p>
 				Go to the <Link to="/products"> list of products</Link>
+			</p>
+			<p>
+				<button onClick={navigateHandler}>navigate</button>
 			</p>
 		</>
 	);
