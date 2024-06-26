74c2e20c00060e0fe879d75e48d30b2332945ac6
Neal Matta <nlsima97@gmail.com>
2024-06-26 18:17:29 -0500

    Cleaner loader data usage

--- 21.1 Advanced Routing Project/frontend/src/pages/EventsPage.jsx
+++ 21.1 Advanced Routing Project/frontend/src/pages/EventsPage.jsx
@@ -1,39 +1,12 @@
-import { useEffect, useState } from 'react';
-
+import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 // Right now, the request is only sent when you reach the eventsPage
 // It works for today but is suboptimal if there are a lot of components and nested components
 function EventsPage() {
-	const [isLoading, setIsLoading] = useState(false);
-	const [fetchedEvents, setFetchedEvents] = useState();
-	const [error, setError] = useState();
+	const events = useLoaderData();
 
-	useEffect(() => {
-		async function fetchEvents() {
-			setIsLoading(true);
-			// const response = await fetch('http://localhost:8080/events');
-
-			// if (!response.ok) {
-			// 	setError('Fetching events failed.');
-			// } else {
-			// 	const resData = await response.json();
-			// 	setFetchedEvents(resData.events);
-			// }
-			setIsLoading(false);
-		}
-
-		fetchEvents();
-	}, []);
-	return (
-		<>
-			<div style={{ textAlign: 'center' }}>
-				{isLoading && <p>Loading...</p>}
-				{error && <p>{error}</p>}
-			</div>
-			{!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
-		</>
-	);
+	return <>{<EventsList events={events} />}</>;
 }
 
 export default EventsPage;
