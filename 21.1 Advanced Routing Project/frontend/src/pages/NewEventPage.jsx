ec5c558b8e2d6419126ca34d8829bb2091e3617a
Neal Matta <nlsima97@gmail.com>
2024-07-08 22:54:04 -0500

    Learned about how to use actions. These are similar to loaders but can be used when submitting forms

--- 21.1 Advanced Routing Project/frontend/src/pages/NewEventPage.jsx
+++ 21.1 Advanced Routing Project/frontend/src/pages/NewEventPage.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import { json, redirect } from 'react-router-dom';
+import EventForm from '../components/EventForm';
 
 export default function NewEventPage() {
-	return <div>NewEventPage</div>;
+	return <EventForm />;
+}
+
+export async function action({ request, params }) {
+	const formData = await request.formData();
+	const eventData = {
+		title: formData.get('title'),
+		image: formData.get('image'),
+		date: formData.get('date'),
+		description: formData.get('description'),
+	};
+
+	const response = await fetch('http://localhost:8080/events', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(eventData),
+	});
+
+	if (!response.ok) {
+		throw json({ message: 'Could not save event.' }, { status: 500 });
+	}
+
+	return redirect('/events');
 }
