22b5d420092d97b6113821453b062e853be924c8
Neal Matta <nlsima97@gmail.com>
2024-09-12 22:20:36 -0500

    Not many new concepts but rehashing old ones

--- 25 Foodies Starting Project/app/meals/page.jsx
+++ 25 Foodies Starting Project/app/meals/page.jsx
@@ -1,5 +1,23 @@
 import Link from 'next/link';
 
-export default function MealsPage() {
-	return <h1>Meals Page</h1>;
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
+export default function Home() {
+	return (
+		<>
+			<header className={classes.header}>
+				<h1>
+					Delicious Meals, Created <span className={classes.highlight}>by you</span>
+				</h1>
+				<p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+				<p className={classes.cta}>
+					<Link href="/meals/share">Share your Favorite Recipe</Link>
+				</p>
+			</header>
+			<main className={classes.main}>
+				<MealsGrid meals={[]} />
+			</main>
+		</>
+	);
 }
