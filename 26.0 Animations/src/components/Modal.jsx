113ee2f7bc154a3f3c38d5cf9dea6e45cc100c21
Neal Matta <nlsima97@gmail.com>
2024-10-09 17:29:29 -0500

    Need to use AnimatePresence to animate exit transitions

--- 26.0 Animations/src/components/Modal.jsx
+++ 26.0 Animations/src/components/Modal.jsx
@@ -11,7 +11,7 @@ export default function Modal({ title, children, onClose }) {
 				// Initial lets you set the starting state and if you deviate from the state, the animate is played
 				initial={{ opacity: 0, y: 30 }}
 				animate={{ opacity: 1, y: 0 }}
-				transition={{ duration: 0.3 }}
+				exit={{ opacity: 0, y: 30 }}
 			>
 				<h2>{title}</h2>
 				{children}
