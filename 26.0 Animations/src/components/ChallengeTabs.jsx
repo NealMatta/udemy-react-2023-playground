8b5bef159aaf6973d1743abcd6bc790ea3f04a6a
Neal Matta <nlsima97@gmail.com>
2024-10-13 21:24:57 -0500

    LayoutID makes the animations of the bars a lot more simple. Need to read documentation around Framer Motion to give me better understanding of how to use it to its full extent

--- 26.0 Animations/src/components/ChallengeTabs.jsx
+++ 26.0 Animations/src/components/ChallengeTabs.jsx
@@ -1,52 +1,47 @@
 import Badge from './Badge.jsx';
+import { motion } from 'framer-motion';
 
 function Tab({ isSelected, onSelect, badgeCaption, children }) {
-  return (
-    <li>
-      <button
-        className={isSelected ? 'selected' : undefined}
-        onClick={onSelect}
-      >
-        {children}
-        <Badge caption={badgeCaption}></Badge>
-      </button>
-      {isSelected && <div className="active-tab-indicator" />}
-    </li>
-  );
+	return (
+		<li>
+			<button className={isSelected ? 'selected' : undefined} onClick={onSelect}>
+				{children}
+				<Badge caption={badgeCaption}></Badge>
+			</button>
+			{/* Having a layout ID means that Framer Motion will automatically look for when 
+      another element in you page has the same ID and then plays an animation to smoothly animate it */}
+			{isSelected && <motion.div layoutId="tab-indicator" className="active-tab-indicator" />}
+		</li>
+	);
 }
 
-export default function ChallengeTabs({
-  selectedType,
-  onSelectType,
-  challenges,
-  children,
-}) {
-  return (
-    <>
-      <menu id="tabs">
-        <Tab
-          isSelected={selectedType === 'active'}
-          onSelect={() => onSelectType('active')}
-          badgeCaption={challenges.active.length}
-        >
-          Active
-        </Tab>
-        <Tab
-          isSelected={selectedType === 'completed'}
-          onSelect={() => onSelectType('completed')}
-          badgeCaption={challenges.completed.length}
-        >
-          Completed
-        </Tab>
-        <Tab
-          isSelected={selectedType === 'failed'}
-          onSelect={() => onSelectType('failed')}
-          badgeCaption={challenges.failed.length}
-        >
-          Failed
-        </Tab>
-      </menu>
-      <div>{children}</div>
-    </>
-  );
+export default function ChallengeTabs({ selectedType, onSelectType, challenges, children }) {
+	return (
+		<>
+			<menu id="tabs">
+				<Tab
+					isSelected={selectedType === 'active'}
+					onSelect={() => onSelectType('active')}
+					badgeCaption={challenges.active.length}
+				>
+					Active
+				</Tab>
+				<Tab
+					isSelected={selectedType === 'completed'}
+					onSelect={() => onSelectType('completed')}
+					badgeCaption={challenges.completed.length}
+				>
+					Completed
+				</Tab>
+				<Tab
+					isSelected={selectedType === 'failed'}
+					onSelect={() => onSelectType('failed')}
+					badgeCaption={challenges.failed.length}
+				>
+					Failed
+				</Tab>
+			</menu>
+			<div>{children}</div>
+		</>
+	);
 }
