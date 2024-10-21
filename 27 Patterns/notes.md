5a2081fd01a187836ca9bb8caa1ee96972d3b387
Neal Matta <nlsima97@gmail.com>
2024-10-20 22:36:55 -0500

    A lot of just adding content

---
+++ 27 Patterns/notes.md
@@ -0,0 +1,4 @@
+# 511 - Introducing Compound Components
+
+-   Components that don't work standalone but only work together
+-   e.g. the <select> and <option> tags must be used together to be used together
--- 27 Patterns/src/App.jsx
+++ 27 Patterns/src/App.jsx
@@ -1,5 +1,28 @@
+import Accordion from './components/Accordion/Accordion';
+import AccordionItem from './components/Accordion/AccordionItem';
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+	return (
+		<main>
+			<section>
+				<h2>Why work with us?</h2>
+				<Accordion className="accordion">
+					<AccordionItem className="accordion-item" title="We got 20 years of experience">
+						<article>
+							<p>You cant go wrong with us</p>
+							<p>We are in the bussiness of planning good trips</p>
+						</article>
+					</AccordionItem>
+					<AccordionItem className="accordion-item" title="We're working with local guides">
+						<article>
+							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+						</article>
+					</AccordionItem>
+				</Accordion>
+			</section>
+		</main>
+	);
 }
 
 export default App;
