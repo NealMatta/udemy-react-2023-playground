9385709be64a4ae448828d59eeb89055205140fe
Neal Matta <nlsima97@gmail.com>
2024-10-21 22:40:51 -0500

    Spent a bit of time relearning what provider and context does

--- 27 Patterns/src/components/Accordion/Accordion.jsx
+++ 27 Patterns/src/components/Accordion/Accordion.jsx
@@ -1,3 +1,38 @@
+import { createContext, useContext, useState } from 'react';
+
+const AccordionContext = createContext();
+
+// Custom hook
+export function useAccordionContext() {
+	const ctx = useContext(AccordionContext);
+
+	if (!ctx) {
+		throw new Error('Accordion-related components must be wrapped by <Accordion>');
+	}
+
+	return ctx;
+}
+
+// This is essentially the provider
 export default function Accordion({ children, className }) {
-	return <ul className={className}>{children}</ul>;
+	const [openItemId, setOpenItemId] = useState();
+	function openItem(id) {
+		setOpenItemId(id);
+	}
+
+	function closeItem() {
+		setOpenItemId(null);
+	}
+
+	const contextValues = {
+		openItemId,
+		openItem,
+		closeItem,
+	};
+
+	return (
+		<AccordionContext.Provider value={contextValues}>
+			<ul className={className}>{children}</ul>
+		</AccordionContext.Provider>
+	);
 }
