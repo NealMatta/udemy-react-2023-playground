e1aee032628b8601d3a1288bdac46c6426ad79d5
Neal Matta <nlsima97@gmail.com>
2024-10-25 19:26:54 -0500

    He did a ton of changes but i am not so sure how important they are to readability. Maybe maintaing these for the long term

--- 27 Patterns/src/App.jsx
+++ 27 Patterns/src/App.jsx
@@ -7,21 +7,27 @@ function App() {
 			<section>
 				<h2>Why work with us?</h2>
 				<Accordion className="accordion">
-					<Accordion.Item id="experience" className="accordion-item" title="We got 20 years of experience">
-						<article>
-							<p>You cant go wrong with us</p>
-							<p>We are in the bussiness of planning good trips</p>
-						</article>
+					<Accordion.Item className="accordion-item">
+						<Accordion.Title id="experience" className="acccordion-item-title">
+							We got 20 years of experience
+						</Accordion.Title>
+						<Accordion.Content id="experience">
+							<article>
+								<p>You cant go wrong with us</p>
+								<p>We are in the bussiness of planning good trips</p>
+							</article>
+						</Accordion.Content>
 					</Accordion.Item>
-					<Accordion.Item
-						id="local-guides"
-						className="accordion-item"
-						title="We're working with local guides"
-					>
-						<article>
-							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
-							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
-						</article>
+					<Accordion.Item className="accordion-item">
+						<Accordion.Title id="local-guides" className="acccordion-item-title">
+							We're working with local guides
+						</Accordion.Title>
+						<Accordion.Content id="local-guides">
+							<article>
+								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+							</article>
+						</Accordion.Content>
 					</Accordion.Item>
 				</Accordion>
 			</section>
