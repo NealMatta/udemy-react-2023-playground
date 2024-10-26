43bf9b4453fc5d3955b024b73ad7dd377f5c67a1
Neal Matta <nlsima97@gmail.com>
2024-10-25 19:45:55 -0500

    Overkill but for the sake of learning, used Context again to reduce the amount of code duplication Id need to do

--- 27 Patterns/src/App.jsx
+++ 27 Patterns/src/App.jsx
@@ -1,31 +1,34 @@
-import Accordion from './components/Accordion/Accordion';
-import AccordionItem from './components/Accordion/AccordionItem';
+import Accordion from './components/Accordion/Accordion.jsx';
 
 function App() {
 	return (
 		<main>
 			<section>
 				<h2>Why work with us?</h2>
+
 				<Accordion className="accordion">
-					<Accordion.Item className="accordion-item">
-						<Accordion.Title id="experience" className="acccordion-item-title">
+					<Accordion.Item id="experience" className="accordion-item">
+						<Accordion.Title className="accordion-item-title">
 							We got 20 years of experience
 						</Accordion.Title>
-						<Accordion.Content id="experience">
+						<Accordion.Content className="accordion-item-content">
 							<article>
-								<p>You cant go wrong with us</p>
-								<p>We are in the bussiness of planning good trips</p>
+								<p>You can&apos;t go wrong with us.</p>
+								<p>
+									We are in the business of planning highly individualized vacation trips for more
+									than 20 years.
+								</p>
 							</article>
 						</Accordion.Content>
 					</Accordion.Item>
-					<Accordion.Item className="accordion-item">
-						<Accordion.Title id="local-guides" className="acccordion-item-title">
-							We're working with local guides
+					<Accordion.Item id="local-guides" className="accordion-item">
+						<Accordion.Title className="accordion-item-title">
+							We are working with local guides
 						</Accordion.Title>
-						<Accordion.Content id="local-guides">
+						<Accordion.Content className="accordion-item-content">
 							<article>
-								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
-								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+								<p>We are not doing this along from our office.</p>
+								<p>Instead, we are working with local guides to ensure a safe and pleasant vacation.</p>
 							</article>
 						</Accordion.Content>
 					</Accordion.Item>
