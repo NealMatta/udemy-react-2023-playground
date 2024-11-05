de283a5cd913836fe978a8bb0c541d613d4df0e3
Neal Matta <nlsima97@gmail.com>
2024-11-05 15:52:10 -0600

    The search values need to be output differently and that is configured via functions

--- 27 Patterns/src/components/SearchableList/SearchableList.jsx
+++ 27 Patterns/src/components/SearchableList/SearchableList.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function SearchableList({ items }) {
+export default function SearchableList({ items, children }) {
 	const [searchTerm, setSearchTerm] = useState('');
 
 	const searchResults = items.filter((item) => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()));
@@ -13,7 +13,8 @@ export default function SearchableList({ items }) {
 			<input type="search" placeholder="Search" onChange={handleChange} />
 			<ul>
 				{searchResults.map((item, index) => (
-					<li key={index}> {item.toString()}</li>
+					// Calling this as a function because you want different text for every item
+					<li key={index}> {children(item)} </li>
 				))}
 			</ul>
 		</div>
