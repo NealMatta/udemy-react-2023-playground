68291c57944d6c82f4f857cc020b636a1fc90929
Neal Matta <nlsima97@gmail.com>
2024-11-13 22:10:01 -0600

    Wrote more intense tests that are starting to help me understand where I can put tests into my platform

--- 29 Testing React Apps/src/components/Greeting.jsx
+++ 29 Testing React Apps/src/components/Greeting.jsx
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+
 export default function Greeting() {
+	const [changedText, setChangedText] = useState(false);
+
+	function changeTextHandler() {
+		setChangedText(true);
+	}
 	return (
 		<>
 			<h2>Greeting</h2>
-			<p>It's good to see you</p>
+			{!changedText && <p>It's good to see you</p>}
+			{changedText && <p>Changed</p>}
+			<button onClick={changeTextHandler} />
 		</>
 	);
 }
