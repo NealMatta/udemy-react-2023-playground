1b74ecc337af2bbf0728124698281945369789fa
Neal Matta <nlsima97@gmail.com>
2024-11-14 20:38:22 -0600

    Primitive types to be aware of

---
+++ 30 React and Typescript/basics.ts
@@ -0,0 +1,8 @@
+// Primitives: number, string, boolean
+// More complex types: array, objects
+// Function types, parameters
+
+// Primitives
+let age: number = 24;
+let userName: string = 'tester';
+let isInstructor: boolean = true;
