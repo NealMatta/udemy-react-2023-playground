ea562f958d424db3c0792a53e479da2e5ba94464
Neal Matta <nlsima97@gmail.com>
2024-11-22 17:42:22 -0600

    Just getting the basics of functional components down

--- 30/30.1 React ts starting project/src/components/Todos.tsx
+++ 30/30.1 React ts starting project/src/components/Todos.tsx
@@ -1,13 +1,16 @@
-function Todos() {
+// What React.FC does is that it defines this function is a functional component
+// It makes it so that I don't have to always import children and define the type
+
+const Todos: React.FC<{ items: string[] }> = (props) => {
 	return (
 		<>
 			<ul>
-				<li>Learn React</li>
-				<li>Learn Typescript</li>
-				<li>Learn Python</li>
+				{props.items.map((item) => (
+					<li key={item}>{item}</li>
+				))}
 			</ul>
 		</>
 	);
-}
+};
 
 export default Todos;
