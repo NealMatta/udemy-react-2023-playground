eff79da83138bb302b2afe2bc871b6ed786f3c59
Neal Matta <nlsima97@gmail.com>
2024-12-01 22:28:37 -0600

    Using classes instead of types and interfaces allows me to add constructors to set up the data as well

--- 30/30.1 React ts starting project/src/components/Todos.tsx
+++ 30/30.1 React ts starting project/src/components/Todos.tsx
@@ -1,12 +1,14 @@
 // What React.FC does is that it defines this function is a functional component
 // It makes it so that I don't have to always import children and define the type
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
+import Todo from '../models/todo';
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
 	return (
 		<>
 			<ul>
-				{props.items.map((item) => (
-					<li key={item}>{item}</li>
+				{props.items.map(({ id, text }) => (
+					<li key={id}>{text}</li>
 				))}
 			</ul>
 		</>
