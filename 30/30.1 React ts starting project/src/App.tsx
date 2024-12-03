f991b7a0b7ddc1a31037603b3c0e8b3fa9e26664
Neal Matta <nlsima97@gmail.com>
2024-12-02 20:00:16 -0600

    Modified state to include typescript

--- 30/30.1 React ts starting project/src/App.tsx
+++ 30/30.1 React ts starting project/src/App.tsx
@@ -2,10 +2,17 @@ import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
-function App() {
-	const todos = [new Todo('Learn React'), new Todo('Learn Typescript')];
+import { useState } from 'react';
 
-	const addTodoHandler = (todoText: string) => {};
+function App() {
+	const [todos, setTodos] = useState<Todo[]>([new Todo('Learn React'), new Todo('Learn Typescript')]);
+
+	const addTodoHandler = (todoText: string) => {
+		const newTodo = new Todo(todoText);
+		setTodos((prevTodos) => {
+			return prevTodos.concat(newTodo);
+		});
+	};
 	return (
 		<div className="App">
 			<NewTodo onAddTodo={addTodoHandler} />
